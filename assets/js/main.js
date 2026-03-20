409c0e74b8519fb01697d61113ee8b6d44f230d2
Carlos Lopez Medina <dew.camalopez@gmail.com>
2026-03-20 23:47:43 +0100

    se realizo la funcion de mostrar y ocultar el boton de buscar en Javascript

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  // FUNCION MOSTRAR Y OCULTAR EL BUSCADOR
+  let input_search = document.querySelector(".search__input");
+  let btn_search = document.querySelector(".navbar__button");
+
+  btn_search.addEventListener("click", () => {
+    let visible = document.querySelector(".visible");
+
+    if (visible) {
+      input_search.style.width = "0";
+      input_search.style.padding = "0";
+      input_search.classList.remove("visible");
+    } else {
+      input_search.style.width = "20rem";
+      input_search.style.padding = "0.5rem 0rem 0.5rem 0.5rem";
+      input_search.classList.add("visible");
+    }
+  });
+});
